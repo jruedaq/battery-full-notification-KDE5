50b0d808987ede568f80426c5f7eb5357b28aaa9
Juan David Rueda Quiroga <jrueda@oneago.com>
2021-03-02 22:40:28 -0500

    Fix sound

--- batteryfull.sh
+++ batteryfull.sh
@@ -5,8 +5,8 @@ while true; do
   battery_percent=$(acpi -b | grep -P -o '[0-9]+(?=%)')
   if on_ac_power; then
     if [ "$battery_percent" -gt 95 ]; then
-      kdialog --title "Backup Complete" --sorry "Battery full.\nLevel: ${battery_percent}% "
       paplay /usr/share/sounds/Oxygen-Im-Nudge.ogg
+      kdialog --title "Backup Complete" --sorry "Battery full.\nLevel: ${battery_percent}% "
     fi
   fi
   sleep 300 # (5 minutes)
